4449a683f822c648937755fd947044ccc7fd9728
Lioncat2002 <gamedevcorp2002@gmail.com>
2021-10-15 13:01:27 +0530

    updates

--- programs/pattern5.py
+++ programs/pattern5.py
@@ -7,11 +7,21 @@
 4333334
 4444444
 '''
-k=int(input("Range:"))
-n=int(input("Limit:"))
-m=n
-for i in range(0,k):
-    for j in range(0,k):
-        pass
-        
+n=int(input())
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        min=i if i<j else j
+        print(n-min+1,end=' ')
+    for j in range(n-1,0,-1):
+        min=i if i<j else j
+        print(n-min+1,end=' ')
+    print()
+for i in range(n-1,0,-1):
+    for j in range(1,n+1):
+        min=i if i<j else j
+        print(n-min+1,end=' ')
+    for j in range(n-1,0,-1):
+        min=i if i<j else j
+        print(n-min+1,end=' ')
     print()
---
+++ programs/pattern6.py
@@ -0,0 +1,22 @@
+'''
+1 2 3 4 5
+11 12 13 14 15
+21 22 23 24 25
+16 17 18 19 20
+6 7 8 9 10
+
+'''
+import math
+n=int(input())
+
+for i in range(0,n):
+    for j in range(0,n):
+        if i< int(math.ceil(n/2)):
+            #pass
+            print((j+1)+10*i,end=' ')
+        elif i== int(math.ceil(n/2)):
+            #pass
+            print((j+1)+(10*(i-1))-n,end=' ')
+        else:
+            print((j+1)+10*(n-i)-n,end=' ')
+    print()
